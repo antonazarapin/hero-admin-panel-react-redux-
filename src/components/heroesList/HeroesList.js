a6737ffee9b5910f77b7af530548e2fff3e20ed1
Anton Azarapin <108542363+antonazarapin@users.noreply.github.com>
2023-03-25 15:12:17 +0200

    Create deleting functional

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,8 +1,8 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDelete } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -31,13 +31,22 @@ const HeroesList = () => {
         return <h5 className="text-center mt-5">Ошибка загрузки</h5>
     }
 
+    const onDelete = (id) => {
+        fetch(`http://localhost:3001/heroes/${id}`, {method: 'DELETE'})
+            .then(dispatch(heroDelete(id)))
+            .catch(error => console.error(error))
+    }
+
     const renderHeroesList = (arr) => {
         if (arr.length === 0) {
             return <h5 className="text-center mt-5">Героев пока нет</h5>
         }
 
         return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
+            return <HeroesListItem 
+                        key={id} 
+                        {...props}
+                        onDelete={() => onDelete(id)}/>
         })
     }
 
